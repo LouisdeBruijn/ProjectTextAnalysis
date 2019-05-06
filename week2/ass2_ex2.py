e51fe43f2359dcc8dd68b8d8146ef9759a7c6eaa
louisdebruijn <l.e.d.de.bruijn@student.rug.nl>
2019-05-06 14:28:01 +0200

    print statements

--- week2/ass2_ex2.py
+++ week2/ass2_ex2.py
@@ -9,6 +9,7 @@ import operator
 
 def main():
 
+    # kan dit weg?
     # Opdracht 1
     # tokens = nltk.word_tokenize("Peter really liked the movies and warm pop-corn . He would never bring Mira with him, though .")
     # print(tokens)
@@ -22,11 +23,11 @@ def main():
     print('sentences:', len(br_tw))
     print('words:', len(br_ts))
 
-    print("### Exercise 2b: 50th and 75th word and POS tag")
+    print("### Exercise 2b: 50th and 75th word and POS-tag")
     print("50th word '{0}' and POS-tag '{1}'".format(br_tw[50][0], br_tw[50][1]))
     print("75th word '{0}' and POS-tag '{1}'".format(br_tw[75][0], br_tw[75][1]))
 
-    print("### Exercise 2c: amount of different POS tags in Brown category")
+    print("### Exercise 2c: amount of different POS-tags in Brown category")
     # Finding the not duplicates part of speech
     not_duplicates = [x[1] for x in br_tw]
     print("There are {0} different POS-tags in Brown category.".format(len(set(not_duplicates))))
@@ -35,42 +36,40 @@ def main():
     print('No idea what this represents:', len(set(br_tw)))
 
     print("### Exercise 2d: top 15 words and counts")
-    # Looking top 15 words
     all_words = [x[0] for x in br_tw]
     print(nltk.FreqDist(all_words).most_common(15))
-    # klopt dit??
 
+    print("### Exercise 2e: top 15 POS-tags and counts")
     # waar is 2e???
 
-    print("### Exercise 2f: ")
+    print("### Exercise 2f: the most frequent POS-tags in sentence X")
     # excersize f just change 20 to 40 if you want to know the other one.
     allwords = [x[0] for x in br_ts[60]]
     print(nltk.FreqDist(allwords).most_common(15))
-    # klopt dit??
+    # dit klopt volgens mij niet
 
     print("### Exercise 2g: most frequent adverb")
-    # excersize g: most common adverb
     allwords2 = [x for x in br_tw if x[1] == "RB"]
     print(nltk.FreqDist(allwords2).most_common(1))
 
     print("### Exercise 2h: most frequent adjective")
-    # excersize h: most frequent adjective
     allwords3 = [x for x in br_tw if x[1] == "JJ"]
     print(nltk.FreqDist(allwords3).most_common(1))
 
-    print("### Exercise 2i: three distinct POS-tags for word 'so'")
+    print("### Exercise 2i: three distinct POS-tags for word 'so' are")
     # excersize i and j: the word so and his part of speech
     allwords = [x for x in br_tw if x[0] == "so"]
     all_words = nltk.ConditionalFreqDist(br_tw)
-    # the three POS-tags: 
     for item in all_words["so"].most_common():
         print(item[0])
 
-    # print(all_words["so"].most_common(1))
+    print("### Exercise 2j: the most frequent POS-tag for the word 'so' is")
+    print("The POS-tag {0} and its count {1}".format(all_words["so"].most_common(1)[0][0], all_words["so"].most_common(1)[0][1]))
+   
+    print("### Exercise 2k: example sentences with POS-tags for word 'so' ")
+    
 
-    print("### Exercise 2j:")
-    print("### Exercise 2k:")
-    print("### Exercise 2l:")
+    print("### Exercise 2l: ")
 
     """ Excersize 3 """
     path = "holmes.txt"
