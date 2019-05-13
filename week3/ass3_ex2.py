0d0c3ee517665fdb56f11a08573020f79ae4f8d6
nickalgra <nickalgra@hotmail.com>
2019-05-13 21:48:42 +0200

    ex2 part 1&2 code

---
+++ week3/ass3_ex2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# File name: ass3_ex2.py
+# Description: script that generates all the information required in exercise 2
+# Author: Louis de Bruijn & Friso Stolk
+# Date: 09-05-2018
+
+import nltk # v3.4
+from nltk.corpus import wordnet as wn
+from nltk.stem.wordnet import WordNetLemmatizer
+from collections import defaultdict
+from nltk.corpus import wordnet_ic
+from nltk.parse import CoreNLPParser
+import operator
+
+
+def main():
+
+    # Read the text
+    path = "ada_lovelace.txt"
+    f = open(path)
+    rawText = f.read()
+    f.close()
+
+    sents = nltk.sent_tokenize(rawText) # tokenize rawText to sentences
+    tokens_s = [nltk.word_tokenize(s) for s in sents] # a list of lists: sentences w/ tokens
+    tokens_t = [t for s in tokens_s for t in s] # a list of tokens 
+    pos_tags = nltk.pos_tag(tokens_t)
+
+    # create a list of nouns (recognized by their POS-tag)
+    nouns = []
+    for i in range(len(pos_tags)):
+        if pos_tags[i][1] == 'NN' or pos_tags[i][1] == 'NNP' or pos_tags[i][1] == 'NNS' or pos_tags[i][1] == 'NNPS':
+            nouns.append(pos_tags[i][0])
+
+    # lemmatize the nouns
+    lemmatizer = WordNetLemmatizer()
+    noun_lemmas = [lemmatizer.lemmatize(noun, wn.NOUN) for noun in nouns]
+
+
+    print("### Exercise 1 & 2, changing depending on server properties")
+    ner_tagger = CoreNLPParser(url='http://localhost:9000', tagtype='ner')
+    nec_list = list(ner_tagger.tag(tokens_t))
+    nec_dict = defaultdict(list)
+    for (name, nec) in nec_list:
+        if not nec == 'O':
+            nec_dict[nec].append(name)
+    for e in nec_dict:
+        print("Named Entity Class '{}' was found {} times: {}".format(e, len(nec_dict[e]), nec_dict[e]))
+
+
+if __name__ == '__main__':
+    main()
