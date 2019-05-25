8fa0562b1d4074731c644e2592b7d85f91a305ce
louisdebruijn <l.e.d.de.bruijn@student.rug.nl>
2019-05-25 11:41:41 +0200

    file for checking annotations

---
+++ week4/annotations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# File name: annotations.py
+# Description: 
+# Author: Louis de Bruijn & Friso Stolk & Nick Algra
+# Date: 25-05-2018
+
+import glob
+import csv
+
+
+def main():
+
+    path_list = glob.glob('group11/*/*/*.tok.off.pos.l')
+    for path in path_list:
+        with open(path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=' ')
+            for line in csv_reader:
+                if len(line) == 6: # no entity class tag ?
+                    print(path, line)
+                elif len(line) == 7:
+                    # print(path[:17], line[5], line[6]) # check correctness
+                    if ' ' in line[6]:
+                        print(line[6]) # whitespaces in link
+                    if ' ' in line[5]:
+                        print(line[5]) # whitespaces in entiy class tag
+
+
+if __name__ == '__main__':
+    main()
