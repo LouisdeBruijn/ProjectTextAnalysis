aec1d89b37e14bc910a7dd1005f61fd9128035f3
nickalgra <nickalgra@hotmail.com>
2019-05-29 15:20:52 +0200

    start of disambiguation.py

---
+++ week6/disambiguation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# File name: disambiguation.py
+# Description: 
+# Author: Louis de Bruijn & Friso Stolk & Nick Algra
+# Date: 29-05-2018
+
+import wikipedia
+from nltk.corpus import wordnet
+from nltk.wsd import lesk
+from nltk import word_tokenize
+
+def main() {
+
+}
+
+if __name__ == "__main__":
+    main()
