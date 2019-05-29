86ef849b18d2c29b8a9e4ff191f56b8a6b880dd8
louisdebruijn <l.e.d.de.bruijn@student.rug.nl>
2019-05-29 16:59:54 +0200

    almost done

--- week6/disambiguation.py
+++ week6/disambiguation.py
@@ -1,61 +1,74 @@
 #!/usr/bin/env python3
 # File name: disambiguation.py
-# Description: 
+# Description: Provide disambiguation for 
 # Author: Louis de Bruijn & Friso Stolk & Nick Algra
 # Date: 29-05-2018
 
 import wikipedia
 import nltk
-from nltk.corpus import wordnet
+from nltk.corpus import wordnet as wn
 from nltk.wsd import lesk
 from collections import defaultdict
 # from nltk import word_tokenize
 # from nltk import pos_tag
 
-def main():
-    PAGE_NAME = "New York City"
-    page = wikipedia.page(PAGE_NAME)
-    raw_text = page.content
-    
-    sents = nltk.sent_tokenize(raw_text) # tokenize rawText to sentences
 
-    # create tokens per sentence, because nltk.word_tokenize needs sentences as input
-    tokens_s = [nltk.word_tokenize(s) for s in sents] # a list of lists: sentences w/ tokens
-    
-    # but for collocations we need not a list of tokens per sentence, but a list of tokens per rawText   
-    tokens_t = [t for s in tokens_s for t in s] # a list of tokens
-    
-    pos_tags = nltk.pos_tag(tokens_t)
-    
-    sent_dict = defaultdict(list)
-    j = k = 0
-    key = sents[0]
-    for i in range(len(pos_tags)):
-        if j == len(tokens_s[k]):
-            k += 1
-            j = 0
-            key = sents[k]
-            sent_dict[key] = []
-        #print(tokens_s[k][j])
-        #print(pos_tags[i])
-        #print('\n')
-        sent_dict[key].append(pos_tags[i])
-        j += 1
+def main():
+
+    wiki_urls = [ 'https://en.wikipedia.org/wiki/North_Waziristan', 
+        'https://en.wikipedia.org/wiki/United_States_Congress', 
+        'https://en.wikipedia.org/wiki/South_Africa', 
+        'https://en.wikipedia.org/wiki/Iraq', 
+        'https://en.wikipedia.org/wiki/Commonwealth_of_Nations'
+        ]
+
+    wiki_names = [ 'North Waziristan', 
+        'United States Congress', 
+        'South Africa', 
+        'Iraq', 
+        'Commonwealth of Nations'
+        ]
+
+    for name in wiki_names:
+        page = wikipedia.page(name)
+        raw_text = page.content
         
-    print(sent_dict)
-    
-    #nouns = []
-    #for i in range(len(pos_tags)):
-    #    if pos_tags[i][1] == 'NN' or pos_tags[i][1] == 'NNP' or pos_tags[i][1] == 'NNS' or pos_tags[i][1] == 'NNPS':
-    #        nouns.append(pos_tags[i][0])
-    
-    #ambiguous_nouns = {}
-    #for noun in nouns:
-    #    if len(wordnet.synsets(noun, 'n')) > 1:
-    #    ambiguous_nouns[noun] = lesk(
+        sents = nltk.sent_tokenize(raw_text) # tokenize rawText to sentences
+        # create tokens per sentence, because nltk.word_tokenize needs sentences as input
+        tokens_s = [nltk.word_tokenize(s) for s in sents] # a list of lists: sentences w/ tokens
+        # but for collocations we need not a list of tokens per sentence, but a list of tokens per rawText   
+        tokens_t = [t for s in tokens_s for t in s] # a list of tokens
+        # POS tagging tokens
+        pos_tags = nltk.pos_tag(tokens_t)
+        
+        # create dictionary with sentences as keys and tokens as value list
+        sent_dict = defaultdict(list)
+        j = k = 0
+        key = sents[0]
+        for i in range(len(pos_tags)):
+            if j == len(tokens_s[k]):
+                k += 1
+                j = 0
+                key = sents[k]
+                sent_dict[key] = []
+            if pos_tags[i][1] == 'NN' or pos_tags[i][1] == 'NNP' or pos_tags[i][1] == 'NNS' or pos_tags[i][1] == 'NNPS':
+                # print(pos_tags[i])
+                sent_dict[key].append(pos_tags[i])
+            j += 1
             
-    #print(len(ambiguous_nouns))
-    
+        # extract ambiguous nouns and find most relevant definiont using Lesk
+        for key, value in sent_dict.items():
+            for (token,pos) in value:
+                # print(token, pos)
+                if len(wn.synsets(token, pos=wn.NOUN)) > 1:
+                    # print(wn.synsets(token, pos=wn.NOUN))
+                    print('For the following word:', token)
+                    print('The Lesk algorithm shows this is the best definition:')
+                    print(lesk(key, token, pos=wn.NOUN))
+                    print()
+
+
 
 if __name__ == "__main__":
     main()
+
